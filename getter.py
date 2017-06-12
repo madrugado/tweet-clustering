fa71941a2134628ae5575a41d2b27d733e1e8440
Valentin Malykh <madrugado@yandex.ru>
2017-06-12 21:33:30 +0300

    ENH: storing to json all the data

--- getter.py
+++ getter.py
@@ -32,7 +32,8 @@ def get_new_tweets(screen_name, last_id=None):
         new_tweets = api.user_timeline(screen_name=screen_name, count=200, since_id=last_id)
 
     # transform the tweepy tweets into a 2D array that will populate the csv
-    outtweets = [(tweet.id_str, tweet.created_at, tweet.text) for tweet in new_tweets]
+    outtweets = [(tweet.id_str, screen_name, tweet.created_at,
+                  tweet.text, tweet.favorite_count, tweet.retweet_count) for tweet in new_tweets]
 
     # write the csv
     with open(TWEETS_PATH, 'at') as f:
@@ -48,7 +49,7 @@ def get_name_and_last_id():
         for line in f:
             l = line.strip()
             if l:
-                names.append(l.strip("/")[-1])
+                names.append(l.split("/")[-1])
 
     if not os.path.exists(LAST_IDS_JSON):
         last_ids = dict([(x, None) for x in names])
@@ -66,10 +67,13 @@ if __name__ == '__main__':
     if not os.path.exists(TWEETS_PATH):
         with open(TWEETS_PATH, "wt") as f:
             writer = csv.writer(f)
-            writer.writerow(["id", "created_at", "text"])
+            writer.writerow(["id", "user_id", "created_at", "text", "retweets", "favorites"])
 
     last_ids = get_name_and_last_id()
     new_last_ids = {}
     for name, last_id in last_ids.items():
         new_last_id = get_new_tweets(name, last_id)
         new_last_ids[name] = new_last_id
+
+    with open(LAST_IDS_JSON, "wt") as f:
+        json.dump(new_last_ids, f)
